6c158846c810d31b954c734bddca1c62ac21b825
ogier <lhm160074lhm@gmail.com>
2022-11-10 14:25:51 +0800

    2022.11.10 144.二叉树前序遍历   无md 不想写QAQ

---
+++ BinTree/144.cpp
@@ -0,0 +1,30 @@
+//
+// Created by ogier on 2022/11/10.
+//
+#include<vector>
+using std::vector;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//前序便利,注意一下传入引用就差不多了
+void travpre(TreeNode *root, vector<int> &ans) {
+  if (!root)
+    return;
+  else
+    ans.push_back(root->val);
+  travpre(root->left, ans);
+  travpre(root->right, ans);
+}
+
+vector<int> preorderTraversal(TreeNode *root) {
+  vector<int> ans;
+  travpre(root, ans);
+  return ans;
+}
