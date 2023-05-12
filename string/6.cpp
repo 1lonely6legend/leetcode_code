2e8e39a6a074e2fe52c7ade6687dd97b26a36f97
ogier_windows <lhm160074lhm@gmail.com>
2023-05-12 20:57:07 +0800

    6. N 字形变换 没做出来

---
+++ string/6.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ogier on 2023/5/12.
+// 6. N 字形变换
+// 看答案,自己的又臭又长还不对 nmmd
+#include <iostream>
+#include <algorithm>
+using std::string;
+
+
+
+int main() {
+  string s = "ABCDE";
+  int num = 4;
+  std::cout << convert(s, num);
+  return 0;
+}
