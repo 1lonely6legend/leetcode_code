f7813b29521e38210f7e5f087a41100c0a65c1b2
ogier_ubuntu <haomiao_li@outlook.com>
2023-06-05 21:45:59 +0800

    705. 设计哈希集合 写出来了,但很慢

--- vector/705.cpp
+++ vector/705.cpp
@@ -1,56 +1,28 @@
 //
-// Created by ogier on 2023/6/1.
+// Created by ogier on 23-6-5.
 // 705. 设计哈希集合
-#include <list>
 #include <vector>
 using std::vector;
-using std::list;
-#define MAX_LEN 100000
 class MyHashSet {
- private:
-  vector<int> set[MAX_LEN];
-
-  /* 返回相应的表索引 */
-  int getIndex(int key) {
-    return key % MAX_LEN;
-  }
-
-  /* 搜索特定存储哈希表中的值。如果值不存在，则返回-1 */
-  int getPos(int key, int index) {
-    for (int i = 0; i < set[index].size(); ++i) {
-      if (set[index][i] == key) {
-        return i;
-      }
-    }
-    return -1;
-  }
  public:
   MyHashSet() {
-  }
-  /* 向哈希集合中插入值key */
-  void add(int key) {
-    int index = getIndex(key);
-    int pos = getPos(key, index);
-    /* 如果不存在则添加新的数据进去 存在不做任何操作 */
-    if (pos < 0) {
-      set[index].push_back(key);
-    }
-  }
-  /* 将给定值key从哈希集合中删除 */
-  void remove(int key) {
-    int index = getIndex(key);
-    int pos = getPos(key, index);
-    if (pos >= 0) {
-      set[index].erase(set[index].begin() + pos);
-    }
+    group.resize(1000001);
   }
 
-  /** 返回哈希集合中是否存在这个值 key */
-  bool contains(int key) {
-    int index = getIndex(key);
-    int pos = getPos(key, index);
-    return pos >= 0;
+  void add(int key) {
+    group[key] = 1;
   }
+
+  void remove(int key) {
+    group[key] = 0;
+  }
+
+  bool contains(int key) {
+    if (group[key]) return true;
+    return false;
+  }
+ private:
+  vector<int> group;
 };
 
 /**
