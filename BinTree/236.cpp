1e4bb0220b8322c32bc3df9d5d49cf31356aa462
ogier_windows <lhm160074lhm@gmail.com>
2023-10-17 22:13:10 +0800

    236.二叉树的最近公共祖先

---
+++ BinTree/236.cpp
@@ -0,0 +1,37 @@
+//
+// Created by ogier on 2023/10/17.
+// 236.二叉树的最近公共祖先
+
+
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right)
+			: val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+	TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+		if (root == q || root == p || root == nullptr) return root;
+		TreeNode *left = lowestCommonAncestor(root->left, p, q);
+		TreeNode *right = lowestCommonAncestor(root->right, p, q);
+		if (left != nullptr && right != nullptr) return root;
+		if (left == nullptr) return right;
+		return left;
+	}
+};
+
+int main() {
+
+	return 0;
+}
