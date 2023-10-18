52ef49ab09739552c2425732592ce496e7054fe5
ogier_windows <lhm160074lhm@gmail.com>
2023-10-18 16:25:47 +0800

    701.二叉搜索树中的插入操作

---
+++ BinTree/701.cpp
@@ -0,0 +1,35 @@
+//
+// Created by ogier on 2023/10/18.
+// 701.二叉搜索树中的插入操作
+
+//关键是压根不需要重构搜索树
+
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right)
+			: val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+	TreeNode *insertIntoBST(TreeNode *root, int val) {
+		if (root == nullptr) {
+			TreeNode *node = new TreeNode(val);
+			return node;
+		}
+		if (root->val > val) root->left = insertIntoBST(root->left, val);
+		if (root->val < val) root->right = insertIntoBST(root->right, val);
+		return root;
+	}
+};
+
