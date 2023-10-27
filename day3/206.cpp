b83ed9780c81b384bfb6e6f71395d74d4ff5704a
ogier_windows <lhm160074lhm@gmail.com>
2023-10-27 16:50:19 +0800

    206.反转链表

--- day3/206.cpp
+++ day3/206.cpp
@@ -1,3 +1,28 @@
 //
 // Created by ogier on 2023/10/27.
-// 206 反转链表
+// 206.反转链表
+#include <iostream>
+using namespace std;
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+	ListNode *reverseList(ListNode *head) {
+		ListNode *cur = head;
+		ListNode *pre = nullptr;
+		ListNode *next = nullptr;
+		while (cur) {
+			next = cur->next;
+			cur->next = pre;
+			pre = cur;
+			cur = next;
+		}
+		return pre;
+	}
+};
