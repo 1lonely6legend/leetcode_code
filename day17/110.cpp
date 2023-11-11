e37eb77aa9818d0fb4ac2da3d4af15d80916345d
1lonely6legend <53079844+1lonely6legend@users.noreply.github.com>
2023-11-11 07:08:32 +0000

    format code day17

--- day17/110.cpp
+++ day17/110.cpp
@@ -1,28 +1,28 @@
 // creat by ogier 2023/11/10
 // 110.平衡二叉树
-#include<iostream>
-#include<queue>
-#include<stack>
+#include <iostream>
+#include <queue>
+#include <stack>
 using namespace std;
 struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
-class Solution{
-    public:
-        int getHeight(TreeNode* node){
-            if(node == nullptr) return 0;
-            int leftHeight = getHeight(node->left);
-            if(leftHeight == -1) return -1;
-            int rightHeight = getHeight(node->right);
-            if(rightHeight == -1) return -1;
-            return abs(leftHeight- rightHeight)>1?-1:1+max(leftHeight,rightHeight);
-            }
-        bool isBalanced(TreeNode* root){
-            return getHeight(root) == -1;
-        }
+class Solution {
+ public:
+  int getHeight(TreeNode *node) {
+    if (node == nullptr) return 0;
+    int leftHeight = getHeight(node->left);
+    if (leftHeight == -1) return -1;
+    int rightHeight = getHeight(node->right);
+    if (rightHeight == -1) return -1;
+    return abs(leftHeight - rightHeight) > 1 ? -1
+                                             : 1 + max(leftHeight, rightHeight);
+  }
+  bool isBalanced(TreeNode *root) { return getHeight(root) == -1; }
 };
--- day17/257.cpp
+++ day17/257.cpp
@@ -3,56 +3,48 @@
 #include <iostream>
 #include <queue>
 #include <stack>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
-class Solution
-{
-public:
-    void traversal(TreeNode *cur, vector<int> path, vector<string> &result)
-    {
-        path.push_back(cur->val);
-        if (cur->left == nullptr && cur->right == nullptr)
-        {
-            string sPath;
-            for (int i = 0; i < path.size() - 1; ++i)
-            {
-                sPath += to_string(path[i]);
-                sPath += "->";
-            }
-            sPath += to_string(path[path.size() - 1]);
-            result.push_back(sPath);
-            return;
-            if (cur->left)
-            {
-                // 如果当前节点的左子树存在
-                traversal(cur->left, path, result);
-                path.pop_back();
-            }
-            if (cur->right)
-            {
-                traversal(cur->right, path, result);
-                path.pop_back();
-            }
-        }
+class Solution {
+ public:
+  void traversal(TreeNode *cur, vector<int> path, vector<string> &result) {
+    path.push_back(cur->val);
+    if (cur->left == nullptr && cur->right == nullptr) {
+      string sPath;
+      for (int i = 0; i < path.size() - 1; ++i) {
+        sPath += to_string(path[i]);
+        sPath += "->";
+      }
+      sPath += to_string(path[path.size() - 1]);
+      result.push_back(sPath);
+      return;
+      if (cur->left) {
+        // 如果当前节点的左子树存在
+        traversal(cur->left, path, result);
+        path.pop_back();
+      }
+      if (cur->right) {
+        traversal(cur->right, path, result);
+        path.pop_back();
+      }
     }
-    vector<string> binaryTreePaths(TreeNode *root)
-    {
-        vector<string> result; // 存放最终字符串
-        vector<int> path;      // 存放遍历过程中的访问节点，使用int便于回溯
-        if (root == nullptr)
-            return result;
-        // 需要先判断root是否唯空，在traversal中默认cur节点非空
-        traversal(root, path, result);
-        return result;
-    }
+  }
+  vector<string> binaryTreePaths(TreeNode *root) {
+    vector<string> result;  // 存放最终字符串
+    vector<int> path;  // 存放遍历过程中的访问节点，使用int便于回溯
+    if (root == nullptr) return result;
+    // 需要先判断root是否唯空，在traversal中默认cur节点非空
+    traversal(root, path, result);
+    return result;
+  }
 };
