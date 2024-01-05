f5fb258c8cbc27892af9521a143564ff8b163532
ogier_ubuntu <haomiao_li@outlook.com>
2024-01-05 21:25:05 +0800

    27移除元素，双指针

---
+++ day1/v2/27.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+  int removeElement(vector<int> &nums, int val) {
+    int slow = 0;
+    for (int fast = 0; fast < nums.size(); fast++) {
+      if (nums[fast] != val)
+        nums[slow++] = nums[fast];
+    }
+    return slow;
+  }
+};
+
