e3009c7050be0c60f9d88926eef661cce6cf66a8
ogier_windows <haomiao_li@outlook.com>
2024-04-07 13:19:09 +0800

    144. 二叉树的前序遍历

---
+++ 144.cpp
@@ -0,0 +1,31 @@
+//
+// Created by ogier on 2024/4/7.
+//
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  void travel(TreeNode *node, vector<int> &res) {
+    if (node == nullptr)
+      return;
+    res.push_back(node->val);
+    travel(node->left, res);
+    travel(node->right, res);
+  }
+  vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> res;
+    travel(root, res);
+    return res;
+  }
+};
