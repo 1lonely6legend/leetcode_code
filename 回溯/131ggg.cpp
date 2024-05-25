a7a51e897c5c15dee44981dc0c87040601cc7090
ogier_ubuntu <haomiao_li@outlook.com>
2024-05-25 15:22:48 +0800

    小小n皇后，不过如此

--- 回溯/131ggg.cpp
+++ 回溯/131ggg.cpp
@@ -1,41 +1,47 @@
 //
 // Created by ogier on 24-5-15.
 //
+#include <functional>
 #include <string>
 #include <vector>
 using namespace std;
 class Solution {
- private:
-  vector<string> path;
-  vector<vector<string>> res;
-  void backtrack(string &s, int start) {
-    if (start >= s.size()) {
-      res.push_back(path);
-    }
-    for (int i = start; i < s.size(); ++i) {
-      if (huiwen(s,start,i)) {
-        string str = s.substr(start,i-start+1);
-        path.push_back(str);
-      }else {
-        continue;
-      }
-      backtrack(s,i+1);
-      path.pop_back();
-    }
-  }
-  bool huiwen(const string &s, int start, int end) {
-    for (int i = start, j = end; i < j; ++i, --j) {
-      if (s[i] != s[j])
-        return false;
+ public:
+  static bool IsPalindrome(string &s, int l, int r) {
+    while (l < r) {
+      if (s[l++] != s[r--]) return false;
     }
     return true;
   }
-
- public:
   vector<vector<string>> partition(string s) {
-    path.clear();
-    res.clear();
-    backtrack(s, 0);
+    vector<vector<string>> res;
+    vector<string> path;
+    function<void(int)> dfs = [&](int start) {
+      if (start == s.size()) {
+        res.emplace_back(path);
+        return;
+      }
+      for (int i = start; i < s.size(); ++i) {
+        if (IsPalindrome(s, start, i)) {
+          path.emplace_back(s.substr(start, i - start + 1));
+          dfs(i + 1);
+          path.pop_back();
+        }
+      }
+    };
+    dfs(0);
     return res;
   }
 };
+int main() {
+  string s = "aab";
+    Solution sol;
+  auto res = sol.partition(s);
+    for (auto &v : res) {
+        for (auto &str : v) {
+        printf("%s ", str.c_str());
+        }
+        printf("\n");
+    }
+  return 0;
+}
--- 回溯/51.cpp
+++ 回溯/51.cpp
@@ -1,3 +1,48 @@
 //
 // Created by ogier on 24-5-15.
 //
+#include <functional>
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+ public:
+  static bool mizi_test(vector<string> &map, int row, int col) {
+    //米字路线上有q时，返回true
+    int size = map.size();
+    //这里直接设置好八个方向的移动
+    vector<array<int, 2>> moves = {{1, 1}, {-1, -1}, {1, -1}, {-1, 1}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    for (auto move : moves) {//遍历八个方向如果，在map范围内，判断是否等于Q
+      int nx = row;
+      int ny = col;//每次都要初始化为当前的位置
+      while (true) {
+        nx +=  move[0];
+        ny +=  move[1];//每次都要加上移动的距离
+        if (nx < 0 || nx >= size || ny < 0 || ny >= size) {
+          break;//这里是||,当越界时，直接break
+        }
+        if (map[nx][ny] == 'Q') return true;//如果当前遇到了Q，返回true
+      }
+    }
+    return false;
+  }
+  vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> res;
+    vector<string> map(n, string(n, '.'));//初始化好n*n的map，用.填充
+    function<void(int)> dfs = [&](int row) {
+      if (row == n) {//这里应该设置为n，而不是n-1，因为最后递归到最后一行之后，再加一，这时候row=n，就是n行了
+        res.emplace_back(map);
+        return;
+      }
+      for (int i = 0; i < n; ++i) {
+        //这里就是一个简单的回溯，如果当前位置可以放Q，就放上Q，然后递归下一行，如果不能放，就继续下一个位置
+        if (mizi_test(map, row, i)) continue;
+        map[row][i] = 'Q';
+        dfs(row + 1);
+        map[row][i] = '.';
+      }
+    };
+    dfs(0);
+    return res;
+  }
+};
