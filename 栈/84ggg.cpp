5b2cd035008edf8b82cd8be183fce851678e0c28
ogier_ubuntu <haomiao_li@outlook.com>
2024-05-25 20:12:24 +0800

    更新最大矩形面积

--- 栈/84ggg.cpp
+++ 栈/84ggg.cpp
@@ -7,28 +7,33 @@
 #include<vector>
 #include <algorithm>
 using namespace std;
+//说白了，这题考的基础模型其实就是：在一维数组中对每一个数找到第一个比自己小的元素。
+//这类“在一维数组中找第一个满足某种条件的数”的场景就是典型的单调栈应用场景
 class Solution {
-  public:
-    int largestRectangleArea(vector<int> &heights) {
-      heights.push_back(0); //首先在heights数组末尾添加一个0，这样可以保证所有的元素都可以被弹出
-      stack<int> stk;
-      int maxArea = 0;
-      for (int i = 0; i < heights.size(); i++) {
-        //遍历heights数组
-        while (!stk.empty() && heights[i] < heights[stk.top()]) {
-          //维护一个单调递增栈
-          int top = stk.top();
-          stk.pop();
-          //最大面积等于当前柱子高度乘以栈顶元素的下标减去栈顶下一个元素的下标
-          //右边沿：正好是i（由于单调栈的性质，第i个柱子就是右边第一个矮于A的柱子）
-          //左边沿：单调栈中紧邻A的柱子。（如果A已经出栈，那么左边沿就是A出栈后的栈顶）而且是该柱子的右边，所以要+1.
-          //当A出栈后，单调栈为空时，那就是说明，A的左边没有比它矮的。左边沿就可以到0.
-          maxArea = max(maxArea, heights[top] * (stk.empty() ? i : (i - stk.top() - 1)));
-        }
-        stk.push(i);
+  //对数组中的每个元素，若假定以它为高，能够展开的宽度越宽，那么以它为高的矩形面积就越大。
+  //因此，思路就是找到每个元素左边第一个比它矮的矩形和右边第一个比它矮的矩形，在这中间的就是最大宽度
+  //最后对每个元素遍历一遍找到最大值即可。
+public:
+  int largestRectangleArea(vector<int> &heights) {
+    heights.push_back(0);
+    stack<int> stk;
+    int res = 0;
+    for(int i = 0; i < heights.size(); ++i) {
+      while(!stk.empty() && heights[i] < heights[stk.top()]) {
+        //一个单调递增栈，栈中存放的是柱子的下标
+        int tmp = stk.top();
+        stk.pop();
+        //怎么说呢，就是当这个数递增时，我们不去计算最大值，因为大肯定还在后面
+        //当前这个数小于栈顶元素时，我们就可以计算了，因为这个数是短板因为肯定比栈顶元素小
+        //所以我们可以计算以栈顶元素为高度的矩形的最大面积
+        //再弹出之后，如果栈为空，说明这个数是最小的，所以宽度就是i
+        //如果栈不为空，那么宽度就是i - 栈顶元素 - 1
+        res = max(res,heights[tmp] * (stk.empty()? i : (i - stk.top() - 1)));
       }
-      return maxArea;
+      stk.push(i);
     }
+    return res;
+  }
 };
 int main() {
   vector<int> test = {2, 1, 5, 6, 2, 3};
